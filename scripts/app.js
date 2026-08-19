5c82d61dfbd67df1874f7b86c12dac87ca8a2d6f
jojoyangupupup <jojoyangupupup@users.noreply.github.com>
2026-08-19 10:17:34 +0800

    Revert "Update doorstep reference image"
    This reverts commit af1189e265b26f54cea6a9ed224bd18cc87539f4.

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,7 @@ import {
   SHOW_HOTSPOTS,
   adjacentPages,
   roomFromPath,
-} from "/scripts/rooms-data.js?v=84";
+} from "/scripts/rooms-data.js?v=83";
 
 const FOCUS_TIMING = {
   expandStart: 80,
