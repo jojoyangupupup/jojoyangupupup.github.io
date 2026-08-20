b27da1e6ffc9c64b58ced8ab7aa3343eff60d720
jojoyangupupup <jojoyangupupup@gmail.com>
2026-08-20 15:02:52 +0800

    Replace doorstep house image

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,7 @@ import {
   SHOW_HOTSPOTS,
   adjacentPages,
   roomFromPath,
-} from "/scripts/rooms-data.js?v=85";
+} from "/scripts/rooms-data.js?v=86";
 
 const FOCUS_TIMING = {
   expandStart: 80,
